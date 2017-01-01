43e02da7513e68e9324d42739e6f5bb2c97342ab
Charlotte Couttie <cpcouttie@gmail.com>
2017-01-02 11:23:42 +1300

    front tile: almost

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+lightbox = new fusepump.lightbox.buynow(1111);
+var callToActionElement = document.getElementById('unique-btn');
+callToActionElement.addEventListener('click', function () {
+  lightbox.show(img); //Show the lightbox
+});
+//# sourceMappingURL=main.js.map
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 
 lightbox = new fusepump.lightbox.buynow(1111);
-var callToActionElement = document.getElementById( "unique-btn");
- callToActionElement.addEventListener("click", function()
-{ lightbox.show(img); //Show the lightbox } );
+var callToActionElement = document.getElementById( 'unique-btn');
+ callToActionElement.addEventListener('click', function()
+{ lightbox.show(img); //Show the lightbox
+} );
